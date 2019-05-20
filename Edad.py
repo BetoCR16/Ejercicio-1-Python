3ec4c1c867062bbb9c429389f421e0daefc5263c
Roberto Carlos Mendez Lopez <B94788@ecci.ucr.ac.cr>
2019-05-20 08:48:10 -0600

    Agrega instrucción "while"

--- Edad.py
+++ Edad.py
@@ -1,13 +1,16 @@
-nombre=input("Escriba su nombre: ")
-print("Hola "+nombre)
-edad = input("Digite su edad: ")
-edadNumero=int(edad)
-#edadNumero=int(input("Digite su edad: ")
-if edadNumero >= 18 :
-    print("¡Eres mayor de edad!")
-    print("Puedes tomar legalmente :D")
-else:
-    print("Eres menor")
-    print("Se buen niño")
+respuesta = "sí"
+
+while (respuesta == "sí"):
+    nombre=input("Escriba su nombre: ")
+    print("Hola "+nombre)
+    edad = input("Digite su edad: ")
+    edadNumero=int(edad)
+    #edadNumero=int(input("Digite su edad: ")
+    if edadNumero >= 18 :
+        print("¡Eres mayor de edad!")
+        print("Puedes tomar legalmente :D")
+    else:
+        print("Eres menor")
+        print("Se buen niño")
 
 print ("Adiós")
