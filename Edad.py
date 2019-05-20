c0da3fbd1d736ccaa52eeb966ebaf24550b9572f
Roberto Carlos Mendez Lopez <B94788@ecci.ucr.ac.cr>
2019-05-20 08:36:53 -0600

    Agrega condiciones

---
+++ Edad.py
@@ -0,0 +1,12 @@
+nombre=input("Escriba su nombre: ")
+print("Hola "+nombre)
+edad = input("Digite su edad: ")
+edadNumero=int(edad)
+#edadNumero=int(input("Digite su edad: ")
+if edadNumero >= 18 :
+    print("¡Eres mayor de edad!")
+    print("Puedes tomar legalmente :D")
+else:
+    print("Eres menor")
+    print("Se buen niño")
+print ("Adiós")
