bd56e2daafb21bdd63105ddf33fec49324b9bc85
Roberto Carlos Mendez Lopez <B94788@ecci.ucr.ac.cr>
2019-05-20 09:46:36 -0600

    Se simplificó la lista

--- Edad.py
+++ Edad.py
@@ -1,20 +1,13 @@
-respuesta = "1"
-nombre=input("Escriba su nombre: ")
 listaEdades = []
-print("Hola "+nombre)
-while (respuesta == "1"):
+respuesta = len(listaEdades)
+nombre=input("Escriba su nombre: ")
+print("Hola "+ nombre)
+while (len(listaEdades) <= 10):
     edad = input("Digite una edad: ")
     edadNumero=int(edad)
     listaEdades.append(edadNumero)
-    if edadNumero >= 18 :
-        print("¡Eres mayor de edad!")
-        print("Puedes tomar legalmente :D")
-    else:
-        print("Eres menor")
-        print("Se buen niño")
-    respuesta = input("¿Otra vez? ( 1:sí / 2:no ): ")
 
-print ("Adiós")
+print ("Listo")
 
 #-------------------------------------- Promediar edades ------------------------------
 suma = 0
@@ -22,5 +15,5 @@ cantidadesDeEdades = len(listaEdades)
 for edad in range(0, cantidadesDeEdades):
     suma = suma + listaEdades[edad]
 promedio = suma / len(listaEdades)
-print("El prmedio de edades es " + promedio)
+print("El prmedio de edades es " + str(promedio))
 
