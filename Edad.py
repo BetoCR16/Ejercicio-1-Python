a9c0c19847e70497f989dc3473278702b03fa442
Roberto Méndez <rcarlosml16@gmail.com>
2019-05-20 18:36:30 -0600

    Fix an error
    Fix error and change condition of "while"

--- Edad.py
+++ Edad.py
@@ -2,7 +2,7 @@ listaEdades = []
 respuesta = len(listaEdades)
 nombre=input("Escriba su nombre: ")
 print("Hola "+ nombre)
-while (len(listaEdades) <= 10):
+while (len(listaEdades) <= 5):
     edad = input("Digite una edad: ")
     edadNumero=int(edad)
     listaEdades.append(edadNumero)
@@ -16,4 +16,4 @@ for edad in range(0, cantidadesDeEdades):
     suma = suma + listaEdades[edad]
 promedio = suma / len(listaEdades)
 print("El promedio de edades es " + str(promedio))
-print("Muchas gracias"
+print("Muchas gracias")
