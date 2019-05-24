031141909df90de65afd08f4d1a8bc15320105c7
Roberto Méndez <rcarlosml16@gmail.com>
2019-05-23 22:13:51 -0600

    Opción 1 lista, fallo en sumatoria

--- Ejercicio_2.py
+++ Ejercicio_2.py
@@ -25,3 +25,16 @@ def dividirNumeros(num1, num2):
 
 opción = input ('Escoja una opción:\n 1. Ingresar 2 números\n 2. Calcular sumatoria\n 3. Convertir de kg a libras\n 4. Calcular de libras a kilogramos\n 5. Dividir los números\n 6. Salir\n \n Su opción: ')
 
+while (opción != '6'):
+    if opción == '1':
+        num1 = int(input('Introduzca el primer número: '))
+        num2 = int(input('Introduzca el segundo número: '))
+        print('LISTO')
+        opción = input ('\n \nEscoja una opción:\n 1. Ingresar 2 números\n 2. Calcular sumatoria\n 3. Convertir de kg a libras\n 4. Calcular de libras a kilogramos\n 5. Dividir los números\n 6. Salir\n \n Su opción: ')
+    elif opción == '2':
+        print(calcularSumatoria(num1, num2))
+        print("LISTO")
+        
+
+
+
