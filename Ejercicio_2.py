1277856326c2a72edc35c5eb8d85b470ee70fa8b
Roberto Méndez <rcarlosml16@gmail.com>
2019-05-23 21:45:54 -0600

    Añade función de división

--- Ejercicio_2.py
+++ Ejercicio_2.py
@@ -1,5 +1,7 @@
 #print ('Escoja una opción:\n 1. Ingresar 2 números\n 2. Calcular sumatoria\n 3. Convertir de kg a libras\n 4. Calcular de libras a kilogramos\n 5. Dividir los números\n 6. Salir')
 
+#------------------------------ Funciones --------------------------------------------
+
 def calcularSumatoria(num1, num2):
     suma = 0
     for valor in range(num1, num2):
@@ -14,3 +16,9 @@ def convertirKg_Lb(num1):
     resultado = num1 * 2.205
     return resultado
 
+def dividirNumeros(num1, num2):
+    if num2 == 0:
+        print('Error, Segundo número no puede ser cero')
+    else:
+        solucion = num1 / num2
+    return solucion
