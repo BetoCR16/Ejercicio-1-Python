d2dad5b6c6eab5e22cdec84abd4086010d698452
Roberto Méndez <rcarlosml16@gmail.com>
2019-05-23 22:41:29 -0600

    Sumatoria funciona correctamente
    Introducir número y la sumatoria funcionan pero no se ha hecho el caso en que las variables para la sumatoria no se hayan introducido

--- Ejercicio_2.py
+++ Ejercicio_2.py
@@ -32,9 +32,11 @@ while (opción != '6'):
     if opción == '1':
         num1 = int(input('Introduzca el primer número: '))
         num2 = int(input('Introduzca el segundo número: '))
-        print('LISTO')
+        print('\n \nLISTO\n \n')
         opción = input ('Escoja una opción:\n 1. Ingresar 2 números\n 2. Calcular sumatoria\n 3. Convertir de kg a libras\n 4. Calcular de libras a kilogramos\n 5. Dividir los números\n 6. Salir\n \n Su opción: ')
     elif opción == '2':
-        print('El resultado de la sumatoria es: ' + str(calcularSumatoria(num1,num2)))
-        print('LISTO')
+        print('\n\nEl resultado de la sumatoria es: ' + str(calcularSumatoria(num1,num2)))
+        print('\n \nLISTO\n \n')
         opción = input ('Escoja una opción:\n 1. Ingresar 2 números\n 2. Calcular sumatoria\n 3. Convertir de kg a libras\n 4. Calcular de libras a kilogramos\n 5. Dividir los números\n 6. Salir\n \n Su opción: ')
+
+print('Muchas gracias')
