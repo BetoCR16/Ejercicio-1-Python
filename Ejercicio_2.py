5db2cf7feba0835d889a916ee77d5abb35100499
Roberto Méndez <rcarlosml16@gmail.com>
2019-05-23 23:39:27 -0600

    Finalizado

--- Ejercicio_2.py
+++ Ejercicio_2.py
@@ -1,6 +1,6 @@
 #------------------------------ Funciones --------------------------------------------
 def introducirOpción():
-    opción = input ('Escoja una opción:\n 1. Ingresar 2 números\n 2. Calcular sumatoria\n 3. Convertir de libras a kg\n 4. Convertir de kg a libras\n 5. Dividir los números\n 6. Salir\n \n Su opción: ')
+    opción = input ('\nEscoja una opción:\n 1. Ingresar 2 números\n 2. Calcular sumatoria\n 3. Convertir de libras a kg\n 4. Convertir de kg a libras\n 5. Dividir los números\n 6. Salir\n \n Su opción: ')
     return opción
 
 def calcularSumatoria(num1, num2):
@@ -18,10 +18,7 @@ def convertirKg_Lb(num1):
     return resultado
 
 def dividirNumeros(num1, num2):
-    if num2 == 0:
-        print('Error, Segundo número no puede ser cero')
-    else:
-        solucion = num1 / num2
+    solucion = num1 / num2
     return solucion
 
 #----------------------------------------- Programa Principal -------------------------------------------------------
@@ -29,26 +26,30 @@ def dividirNumeros(num1, num2):
 opción = input ('Escoja una opción:\n 1. Ingresar 2 números\n 2. Calcular sumatoria\n 3. Convertir de kg a libras\n 4. Calcular de libras a kilogramos\n 5. Dividir los números\n 6. Salir\n \n Su opción: ')
 
 while (opción != '6'):
+#------------------------------------------ Introducir números ------------------------------------------    
     if opción == '1':
         num1 = int(input('Introduzca el primer número: '))
         num2 = int(input('Introduzca el segundo número: '))
         opción = introducirOpción()
-        print('\nLISTO\n')
-    
+        
+#------------------------------------------ Sumatoria --------------------------------------------------------------    
     elif opción == '2':
         print('\nEl resultado de la sumatoria es: ' + str(calcularSumatoria(num1,num2)) + '\n')
         opción = introducirOpción()
-    
+#------------------------------------------ Conversión lb - kg ---------------------------------------------------    
     elif opción == '3':
         print('\n'+ str(num1) + ' libras equivalen a ' + str(convertirLb_Kg(num1)) + ' kilogramos.\n')
         opción = introducirOpción()
-   
+#----------------------------------------- Conversión kg - lb -------------------------------------   
     elif opción == '4':
         print('\n'+ str(num1) + ' kilogramos equivalen a ' + str(convertirKg_Lb(num1)) + ' libras.\n')
         opción = introducirOpción()
-    
+#----------------------------------------- División ----------------------------------------------------    
     elif opción == '5':
-        print('\nLa resultado de la división es: ' + str(dividirNumeros(num1, num2)))
+        if num2 == 0:
+            print('\nError, Segundo número no puede ser cero\n')
+        else:
+            print('\nLa resultado de la división es: ' + str(dividirNumeros(num1, num2)))
         opción = introducirOpción()
 
-print('\nMuchas gracias')
+print('\nMuchas gracias :D')
